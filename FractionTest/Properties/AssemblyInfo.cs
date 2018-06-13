7ccfed4cb9a413b8e666e0775918de5fef72b916
Idanord <33676867+Idanord@users.noreply.github.com>
2018-06-12 23:46:10 -0700

    Individual Unit Testing
    This is based off the fraction math solution give to us in class. This shows how to do a few simple unit tests

---
+++ FractionTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FractionTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FractionTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("af284909-2a0a-4930-96a1-d04c1d2622a0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FractionTest/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FractionMath;
+
+namespace FractionTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAddition()
+        {
+            Fraction f1 = new Fraction("1/4");
+            Fraction f2 = new Fraction("2/8");
+            Fraction f3 = f1 + f2;
+            Assert.AreEqual(f3.ToString(), "2/4");
+        }
+
+        [TestMethod]
+        public void TestSubtraction()
+        {
+            Fraction f1 = new Fraction("3/4");
+            Fraction f2 = new Fraction("2/8");
+            Fraction f3 = f1 - f2;
+            Assert.AreEqual(f3.ToString(), "2/4");
+        }
+
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            Fraction f1 = new Fraction("1/2");
+            Fraction f2 = new Fraction("1/2");
+            Fraction f3 = f1 * f2;
+            Assert.AreEqual(f3.ToString(), "1/4");
+        }
+
+        [TestMethod]
+        public void TestDivision()
+        {
+            Fraction f1 = new Fraction("1/2");
+            Fraction f2 = new Fraction("1/2");
+            Fraction f3 = f1 / f2;
+            Assert.AreEqual(f3.ToString(), "1/1");
+        }
+
+        [TestMethod]
+        public void TestLessThan()
+        {
+            Fraction f1 = new Fraction("1/8");
+            Fraction f2 = new Fraction("1/2");
+            
+            Assert.IsTrue(f1<f2);
+        }
+
+        [TestMethod]
+        public void TestGreaterThan()
+        {
+            Fraction f1 = new Fraction("1/2");
+            Fraction f2 = new Fraction("1/4");
+
+            Assert.IsTrue(f1 > f2);
+        }
+    }
+}
